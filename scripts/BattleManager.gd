fa60ba2adf5f8694f5da6d5f4d30cfd768987608
Ketoten <ketoten@ketoten.net>
2025-06-19 13:09:34 +0900

    UIなどの調整を実施

--- scripts/BattleManager.gd
+++ scripts/BattleManager.gd
@@ -7,6 +7,8 @@ signal player_turn_started
 @export var player_node_path: NodePath
 @export var enemy_node_path: NodePath
 
+@onready var ui = get_node("/root/BattleScene/CanvasLayer/UI")
+
 var player: Character
 var enemy: Character
 var is_player_turn: bool = true
@@ -20,7 +22,7 @@ func _start_turn():
 	if is_player_turn:
 		_enable_player_input()
 	else:
-		_enemy_action()
+		await _enemy_action()
 		
 func _enable_player_input():
 	# UIボタンを活性化するなど
@@ -29,24 +31,22 @@ func _enable_player_input():
 func on_player_action(action: String):
 	match action:
 		"attack":
-			var damage = player.attack(enemy)
-			print("プレイヤーの攻撃:", damage, "ダメージ")
+			await _player_attack_sequence()
+
 		"defend":
-			player.defend()
-			print("プレイヤーは防御の構えを取った！")
+			await _player_defend_sequence()
 			
 	_check_battle_end()
-	is_player_turn = false
-	_start_turn()
 	
-func _enemy_action():
-	# シンプルに常に攻撃
-	var damage = enemy.attack(player)
-	print("敵の攻撃:", damage, "ダメージ")
-	_check_battle_end()
-	is_player_turn = true
-	_start_turn()
-	
+	if enemy.hp > 0 and player.hp > 0:
+		is_player_turn = false
+		await _enemy_action() # 攻撃演出後に敵ターン
+		_check_battle_end()
+		
+		if enemy.hp > 0 and player.hp > 0:
+			is_player_turn = true
+			_start_turn()
+
 func _check_battle_end():
 	if enemy.hp <= 0:
 		print("勝利！")
@@ -54,3 +54,53 @@ func _check_battle_end():
 	elif player.hp <= 0:
 		print("敗北…")
 		get_tree().paused = true
+
+# プレイヤー攻撃シーケンス
+func _player_attack_sequence() -> void:
+	# 攻撃アニメーション再生
+	ui.set_action_buttons_enabled(false) # ボタン無効化
+	var animePlayer = player.get_node("AnimationPlayer")
+	animePlayer.play("attack")
+	await animePlayer.animation_finished
+	
+	# 敵へのダメージ処理
+	var damage = player.attack(enemy)
+	print("プレイヤーの攻撃:", damage, "ダメージ")
+	
+	# 敵のHPだけ更新
+	ui.update_enemy_hp()
+	await get_tree().create_timer(0.5).timeout
+
+# プレイヤー防御シーケンス
+func _player_defend_sequence() -> void:
+	# 防御アニメーション再生
+	ui.set_action_buttons_enabled(false) # ボタン無効化
+	var animePlayer = player.get_node("AnimationPlayer")
+	animePlayer.play("defend")
+	await animePlayer.animation_finished
+	
+	player.defend()
+	print("プレイヤーは防御の構えを取った！")
+	
+	# 防御時はHP変動なしだが、HP表示だけリフレッシュ
+	ui.update_player_hp()
+	await get_tree().create_timer(0.3).timeout
+	
+	
+# 敵攻撃シーケンス
+func _enemy_action() -> void:
+	# 攻撃アニメーション再生
+	ui.set_action_buttons_enabled(false) # ボタン無効化
+	var animePlayer = enemy.get_node("AnimationPlayer")
+	animePlayer.play("attack")
+	await animePlayer.animation_finished
+	
+	# プレイヤーへのダメージ処理
+	var damage = enemy.attack(player)
+	print("敵の攻撃:", damage, "ダメージ")
+	
+	# プレイヤーのHPだけ更新
+	ui.update_player_hp()
+	await get_tree().create_timer(0.5).timeout
+	
+
--- scripts/UI.gd
+++ scripts/UI.gd
@@ -1,42 +1,53 @@
 extends Control
 
-@onready var battle_manager = get_node("/root/BattleScene").get_node("BattleManager")
-@onready var hp_label_player = $VBoxContainer/PlayerHPLabel
-@onready var hp_label_enemy = $VBoxContainer/EnemyHPLabel
-@onready var btn_attack = $VBoxContainer/HBoxContainer/AttackButton
-@onready var btn_defend = $VBoxContainer/HBoxContainer/DefendButton
+@onready var battle_manager = get_node("/root/BattleScene/BattleManager")
+@onready var player_hp_label = $VBoxContainer/HBoxContainer/PlayerHPVBox/PlayerHPLabel
+@onready var player_hp_bar = $VBoxContainer/HBoxContainer/PlayerHPVBox/PlayerHPBar
+
+@onready var enemy_hp_label = $VBoxContainer/HBoxContainer/EnemyVBox/EnemyHPLabel
+@onready var enemy_hp_bar = $VBoxContainer/HBoxContainer/EnemyVBox/EnemyHPBar
+
+@onready var btn_attack = $VBoxContainer/HBoxContainer2/AttackButton
+@onready var btn_defend = $VBoxContainer/HBoxContainer2/DefendButton
+
 
 func _ready() -> void:
 	# BattleManagerのシグナル接続
 	battle_manager.connect("player_turn_started", Callable(self, "_on_player_turn_started"))
 	
 	# ボタンのpressed()シグナルを接続
-	btn_attack.connect("pressed", Callable(self, "on_attack_button_pressed"))
-	btn_defend.connect("pressed", Callable(self, "on_defend_button_pressed"))
+	btn_attack.pressed.connect(on_attack_button_pressed)
+	btn_defend.pressed.connect(on_defend_button_pressed)
 	
 	# 初期ラベル更新 & ボタン有効化
-	_update_labels()
-	_on_player_turn_started()
+	update_labels()
 	
-	
+# プレイヤーターン開始時にボタンを有効化
 func _on_player_turn_started():
-	btn_attack.disabled = false
-	btn_defend.disabled = false
+	set_action_buttons_enabled(true)
+	
+func set_action_buttons_enabled(enabled: bool):
+	btn_attack.disabled = not enabled
+	btn_defend.disabled = not enabled
 	
 func on_attack_button_pressed():
 	print("▶ Attack ボタンが押されました")  # デバッグ用
-	btn_attack.disabled = true
-	btn_defend.disabled = true
-	battle_manager.on_player_action("attack")
-	_update_labels()
+	set_action_buttons_enabled(false)
+	await battle_manager.on_player_action("attack")
 
 func on_defend_button_pressed():
 	print("▶ Defend ボタンが押されました")  # デバッグ用
-	btn_attack.disabled = true
-	btn_defend.disabled = true
-	battle_manager.on_player_action("defend")
-	_update_labels()
+	set_action_buttons_enabled(false)
+	await battle_manager.on_player_action("defend")
 	
-func _update_labels():
-	hp_label_player.text = "HP: %d" % battle_manager.player.hp
-	hp_label_enemy.text  = "HP: %d" % battle_manager.enemy.hp 
+func update_player_hp():
+	player_hp_label.text = "HP: %d" % battle_manager.player.hp
+	player_hp_bar.value = battle_manager.player.hp
+	
+func update_enemy_hp():
+	enemy_hp_label.text  = "HP: %d" % battle_manager.enemy.hp 
+	enemy_hp_bar.value = battle_manager.enemy.hp
+	
+func update_labels():
+	update_player_hp()
+	update_enemy_hp()
